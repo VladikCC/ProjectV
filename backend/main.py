9677c966e9826e6b04fda72cd4a0ea62e0ed399b
Vladuseek <lutykaf@gmail.com>
2026-05-05 12:04:46 +0300

    fixed ci-cd

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import psycopg2
 import os
+import time
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -14,12 +15,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-conn = psycopg2.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST")
-)
+def connect_with_retry():
+    while True:
+        try:
+            connection = psycopg2.connect(
+                dbname=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                host=os.getenv("DB_HOST")
+            )
+            print("Successfully connected to the database!")
+            return connection
+        except psycopg2.OperationalError:
+            print("Database is not ready yet, retrying in 2 seconds...")
+            time.sleep(2)
+
+conn = connect_with_retry()
 cursor = conn.cursor()
 
 class Score(BaseModel):
